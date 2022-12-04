f7e904ebf34614044b5a3f516941d872e4e06eaa
Bart Massey <bart.massey@gmail.com>
2022-12-04 15:24:38 -0800

    finished part 1

--- soln.rs
+++ soln.rs
@@ -5,11 +5,43 @@
 //! Advent of Code Day 3.  
 //! Bart Massey 2022
 
+use std::collections::HashSet;
+
 use aoc::*;
+
+fn compartment(chars: &[char]) -> HashSet<char> {
+    chars.iter().copied().collect()
+}
+
+fn priority(c: char) -> u32 {
+    let base = if c.is_ascii_lowercase() {
+        'a' as u32
+    } else if c.is_ascii_uppercase() {
+        'A' as u32 - 26
+    } else {
+        panic!("unknown priority: {c}")
+    };
+    c as u32 - base + 1
+}
 
 fn main() {
     match get_part() {
-        Part1 => todo!(),
+        Part1 => {
+            let mut total = 0u32;
+            for line in input_lines() {
+                let line: Vec<char> = line.chars().collect();
+                let nline = line.len();
+                assert!(nline & 1 == 0, "uneven knapsack");
+                let left = compartment(&line[..nline / 2]);
+                let right = compartment(&line[nline / 2..]);
+                let misplaced = left.intersection(&right);
+                let mut elems = misplaced.into_iter();
+                let e = *elems.next().expect("undamaged knapsack");
+                assert!(elems.next().is_none(), "misdamaged knapsack");
+                total += priority(e);
+            }
+            println!("{total}");
+        }
         Part2 => todo!(),
     }
 }
